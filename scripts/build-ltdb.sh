a9000c13a025ce22861733d66f3cb91d9b0883ae
Francis Bond <bond@ieee.org>
2025-05-11 22:43:26 +0200

    only try if there is an ace-config file, move file copy to compile.sh

--- scripts/build-ltdb.sh
+++ scripts/build-ltdb.sh
@@ -17,15 +17,38 @@ if [ ! -d "${LTDBDIR}" ]; then
     git clone --branch "flask" --single-branch  https://github.com/fcbond/ltdb.git "${LTDBDIR}"
 fi
 
+get_toml() {
+  local file="$1"
+  shift
+  local key_expr="$*"
+
+  python -c "
+import toml, sys
+data = toml.load(open('$file', 'r'))
+try:
+    value = data${key_expr}
+    print(value)
+except KeyError:
+    print('')
+"
+}
+
+
+
 ## find METADATA
 files=$(find "${BUILD}" -type f -name "METADATA")
 
 
 for file in $files; do
     echo "Creating ltdb for: $file"
-    ## only make compatible trees
-    python etc/ltdb/scripts/grm2db.py --checkgrm \
-	   --outdir build/DBS "${file}" || true
+    config_rel=$(get_toml "$file" "['ACE_CONFIG_FILE']")
+    if [[ -n "$config_rel" ]]; then
+	## only make compatible trees
+	python etc/ltdb/scripts/grm2db.py --checkgrm \
+	--outdir build/DBS "${file}" || true
+    else
+	echo "⚠️ Skipping: missing ACE_CONFIG_FILE"
+    fi
     echo
 done
 
@@ -35,4 +58,4 @@ find build/DBS -type f -name '*.db' -size +0c -exec du -h {} + | sort -h
 
 echo
 echo "🏗️   Copying to etc/ltdb/web/db/"
-find build/DBS -type f -name '*.dat' -size +0c -exec cp {} etc/ltdb/web/db/ \;
+find build/DBS -type f -name '*.db' -size +0c -exec cp {} etc/ltdb/web/db/ \;
