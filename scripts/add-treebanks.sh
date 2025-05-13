05e2c36daaeeab79c44eb814d3519989d50b5e17
Francis Bond <bond@ieee.org>
2025-05-13 17:55:46 +0200

    add support to download a treebank

---
+++ scripts/add-treebanks.sh
@@ -0,0 +1,6 @@
+### very specific to the SRG
+
+## download 
+wget https://github.com/delph-in/srg/releases/download/v0.3.6/0.3.6-release.zip -P build
+unzip -d build/srg/tsdb build/0.3.6-release.zip
+ln -s 0.3.6-release build/srg/tsdb/gold
